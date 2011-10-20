a5f84ee8af9e303c6ff934d22f97df12f6097536
Rashmi Gandhe <rgandhe@regard-solutions.com>
2011-10-20 16:00:30 +0530

    first commit

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,18 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-		try
-		{
-			require "application/models/Test.php";
- 		$obj1 = new App_Model_Test();
-		$res = $obj1->testModule();
-		echo "res=$res<br>";
-		}
-		catch(exception $e)
-		{
-			echo "in exception".$e->getMessage();
-		}
-	//$this->view->users_arr=$result;
+		
 	
 	}//end of     public function indexAction()
 
